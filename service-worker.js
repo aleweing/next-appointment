322c9efa996c2bfb75a36d24705ec2da672e541b
aleweing <alewein@proton.me>
2026-06-16 23:42:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    Next Appointment — Service Worker
    ========================================= */
 
-const CACHE_NAME = 'next-appointment-v2.1';
+const CACHE_NAME = 'next-appointment-v2.2';
 
 const ASSETS_TO_CACHE = [
   './',
